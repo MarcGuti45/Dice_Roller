7c0f6ad727874d144d037e4291c36af371ff55fa
MarcGuti45 <marc.gutierrez@institutvidreres.cat>
2022-10-03 08:26:56 +0200

    Initial commit

---
+++ app/src/main/java/com/marc/dice_roller/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.marc.dice_roller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+            rollDice2()
+        }
+
+
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+    private fun rollDice2() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView2)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
